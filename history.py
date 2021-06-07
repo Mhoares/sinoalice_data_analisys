ae3808b43f062f3d5c77dd060ef84730fc69fbb3
Mario Hoare Silva <mario.hoare@sansano.usm.cl>
2021-06-07 14:09:11 -0400

    rename variables

--- history.py
+++ history.py
@@ -1,12 +1,13 @@
-from user_data import UsersData
+from userdata import UsersData
 import matplotlib.pyplot as plt
+
 activity = UsersData()
-total_historico = activity.getUsersActivityHistory()
-total_historico_items = total_historico.items()
-total_historico_ordenado = sorted(total_historico_items)
+total_history = activity.getUsersActivityHistory()
+total_history_items = total_history.items()
+total_history_sorted = sorted(total_history_items)
 day =[]
 players =[]
-for to in total_historico_ordenado[0:339]:
+for to in total_history_sorted[0:339]:
   day.append(to[0])
   players.append(to[1])
 plt.plot(day, players)
